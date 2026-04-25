591ba59068f7df59bc1e0c1c8c0717d30ac2de90
Paul Adrieul <paladrieul@users.noreply.github.com>
2026-04-25 18:14:10 +0800

    feat: add main entry point with CLI flags and env config support

---
+++ main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+
+	"github.com/sub2api/sub2api/handler"
+)
+
+const (
+	defaultPort    = 8080
+	defaultHost    = "0.0.0.0"
+	appName        = "sub2api"
+	appVersion     = "1.0.0"
+)
+
+func main() {
+	var (
+		port    int
+		host    string
+		version bool
+	)
+
+	flag.IntVar(&port, "port", getEnvInt("PORT", defaultPort), "Port to listen on")
+	flag.StringVar(&host, "host", getEnv("HOST", defaultHost), "Host to bind to")
+	flag.BoolVar(&version, "version", false, "Print version and exit")
+	flag.Parse()
+
+	if version {
+		fmt.Printf("%s version %s\n", appName, appVersion)
+		os.Exit(0)
+	}
+
+	router := handler.NewRouter()
+
+	addr := fmt.Sprintf("%s:%d", host, port)
+	log.Printf("Starting %s v%s on %s", appName, appVersion, addr)
+
+	server := &http.Server{
+		Addr:    addr,
+		Handler: router,
+	}
+
+	if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		log.Fatalf("Failed to start server: %v", err)
+	}
+}
+
+// getEnv retrieves an environment variable or returns a default value.
+func getEnv(key, defaultValue string) string {
+	if value, ok := os.LookupEnv(key); ok {
+		return value
+	}
+	return defaultValue
+}
+
+// getEnvInt retrieves an integer environment variable or returns a default value.
+func getEnvInt(key string, defaultValue int) int {
+	if value, ok := os.LookupEnv(key); ok {
+		if intVal, err := strconv.Atoi(value); err == nil {
+			return intVal
+		}
+		log.Printf("Warning: invalid integer value for %s, using default %d", key, defaultValue)
+	}
+	return defaultValue
+}
